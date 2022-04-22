26bcf9d38a332d5bb2979914a3acc230825bdab0
Kaladar <ckaladar@gmail.com>
2022-04-22 14:37:33 +0530

    Created function to delete the given node(DIDN'T TESTED FOR MISTAKES

--- Testing.c
+++ Testing.c
@@ -31,87 +31,93 @@ struct node *ip(int data);
 
 //prt(25) returns address of parent of node 25
 //Assuming parent's data is already checked
-struct node *prt(struct node *p, int data){
-	if(p->lchild == NULL && p->rchild == NULL){
-		printf("\n Given node is not present. Ending program abruptly.");
-		exit(1);
-	}
-
-	
-	if((p->lchild != NULL && p->lchild->data == data)||(p->rchild!=NULL && p->rchild->data == data)){
-		return p;
-	}
-	
-	
-	if(data < p->data){
-		return prt(p->lchild, data);
-	}else if(data > p->data){
-		return prt(p->rchild, data);
-	}
-}
+struct node *prt(struct node *p, int data)
 
 //f1(25) returns parent to the smallest node in subtree with root 25
-//Remember special cases
-struct node *f1(int data){
-	struct node *trav = address(root, data);
-	
-	if(trav->lchild == NULL){
-		printf("\n Least child is node itself, ending the program abruptly");
-		exit(0);
-	}
-	
-	while(trav->lchild->lchild != NULL){
-		trav = trav->lchild;		
-	}
-	
-	return trav;
-}
-
+struct node *f1(int data)
 //f2(25) returns the parent of infix successor
-struct node *f2(int data){
-	struct node *trav = address(root, data);
-	
-	if(trav->rchild == NULL){
-		printf("\n Given node doesn't have infix successor. Ending program abruptly");
-		exit(1);
-	}else if(trav->rchild->lchild == NULL){
-		return trav;
-	}
-	
-	return f1(trav->rchild->data);
-}
-
+struct node *f2(int data)
 //g1(25) returns parent of largest node in 25 subtree
-struct node *g1(int data){
-	struct node *trav = address(root, data);
-	
-	if(trav->rchild == NULL){
-		printf("Doesn't have any larger nodes in the subtree. Ending the program abruptly");
-		exit(1);
-	}
-	
-	while(trav->rchild->rchild != NULL){
-		trav = trav->rchild;
-	}
-	
-	return trav;
-}
-
+struct node *g1(int data)
 //g2(25) returns parent of infix predecessor to 25
-struct node *g2(int data){
+struct node *g2(int data)
+
+struct node delete(int data){
+
+	struct node *neww;
+	int left;
+
+
+//  Find the node to be deleted
+	struct node *del = address(root, data);
+//	Find the nodes parent
+	struct node *pt = prt(root, data);
 	
-	struct node *trav = address(root, data);
-	
-	if(trav -> lchild == NULL){
-		printf("\nDoesn't have any infix predecessor. Ending program abruptly");
-		exit(1);
-	}else if(trav->lchild->rchild == NULL){
-		return trav;
-	}else{
-		return g1(trav->lchild->data);
+//Find whether the node to be deleted is left child or not
+	if(pt->lchild == del) left = 1;
+	else left = 0;
+
+//If it doesn't have any children then Nullify
+	if(del->lchild == NULL && del->rchild == NULL){
+		if(left == 1) pt->lchild == NULL;
+		else pt->rchild == NULL;
+		
+		printf("\n %d successfully deleted", data);
+		return;
+
 	}
 	
-	
+//Atleast Left Child
+	if(del->lchild != NULL){
+//		lchild of the del is itself infix predecessor
+		if(del->lchild->rchild == NULL){
+			neww = del->lchild;
+			neww->rchild = del->rchild;
+			neww->lchild = NULL;
+			
+			if(left == 1) pt-lchild == neww;
+			else pt->lchild = neww;
+			
+			printf("\n %d successfully deleted", data);
+			return;			
+		}
+		
+		struct node *ipp = g2(data);
+		struct node *ip = ipp->rchild;
+		ipp->rchild = ip->lchild;
+		
+		ip -> lchild = del -> lchild;
+		ip -> rchild = del -> rchild;
+		
+		if(left == 1) pt -> lchild = neww;
+		else pt -> rchild == neww;
+		
+		printf("\n %d successfully deleted", data);
+		return;	
+		
+		
+	} else {
+		if(del->rchild->lchild == NULL ){
+			if(left == 1) pt->lchild = del->rchild;
+			else pt->rchild = del->rchild;
+			printf("\n %d successfully deleted", data);
+			return;	
+		}
+		
+		struct node *isp = f2(data)
+		struct node *is = isp->lchild;
+		isp->lchild = is->rchild;
+		
+		is->lchild = del->lchild;
+		is->rchild = del->rchild;
+		
+		if(left == 1) pt->lchild = is;
+		else pt->rchild = is;
+		
+		printf("\n %d successfully deleted", data);
+		return;	
+		
+	}
 }
 
 int main(){
@@ -133,14 +139,15 @@ int main(){
 //           address(root, 250)
 	
 // TEST THE NEW FUNCTION HERE
-		printf("\n g2(%d) = %d ",200 ,g2(200)->data);
 
-int i;
-	for(i = 0; i<l ; i++){
-		int test = values[i];
-		printf("\n g2(%d) = %d ",test ,g2(test)->data);
-		
-	}
+
+//		printf("\n g2(%d) = %d ",200 ,g2(200)->data);
+//int i;
+//	for(i = 0; i<l ; i++){
+//		int test = values[i];
+//		printf("\n g2(%d) = %d ",test ,g2(test)->data);
+//		
+//	}
 
 
 
@@ -273,3 +280,91 @@ struct node *ip(int data){
 	} 
 	else return lv(parent->lchild->data);
 }
+
+
+
+//prt(root,25) returns address of parent of node 25
+//Assuming parent's data is already checked
+struct node *prt(struct node *p, int data){
+	if(p->lchild == NULL && p->rchild == NULL){
+		printf("\n Given node is not present. Ending program abruptly.");
+		exit(1);
+	}
+
+	
+	if((p->lchild != NULL && p->lchild->data == data)||(p->rchild!=NULL && p->rchild->data == data)){
+		return p;
+	}
+	
+	
+	if(data < p->data){
+		return prt(p->lchild, data);
+	}else if(data > p->data){
+		return prt(p->rchild, data);
+	}
+}
+
+//f1(25) returns parent to the smallest node in subtree with root 25
+struct node *f1(int data){
+	struct node *trav = address(root, data);
+	
+	if(trav->lchild == NULL){
+		printf("\n Least child is node itself, ending the program abruptly");
+		exit(0);
+	}
+	
+	while(trav->lchild->lchild != NULL){
+		trav = trav->lchild;		
+	}
+	
+	return trav;
+}
+
+//f2(25) returns the parent of infix successor
+struct node *f2(int data){
+	struct node *trav = address(root, data);
+	
+	if(trav->rchild == NULL){
+		printf("\n Given node doesn't have infix successor. Ending program abruptly");
+		exit(1);
+	}else if(trav->rchild->lchild == NULL){
+		return trav;
+	}
+	
+	return f1(trav->rchild->data);
+}
+
+//g1(25) returns parent of largest node in 25 subtree
+struct node *g1(int data){
+	struct node *trav = address(root, data);
+	
+	if(trav->rchild == NULL){
+		printf("Doesn't have any larger nodes in the subtree. Ending the program abruptly");
+		exit(1);
+	}
+	
+	while(trav->rchild->rchild != NULL){
+		trav = trav->rchild;
+	}
+	
+	return trav;
+}
+
+//g2(25) returns parent of infix predecessor to 25
+struct node *g2(int data){
+	
+	struct node *trav = address(root, data);
+	
+	if(trav -> lchild == NULL){
+		printf("\nDoesn't have any infix predecessor. Ending program abruptly");
+		exit(1);
+	}else if(trav->lchild->rchild == NULL){
+		return trav;
+	}else{
+		return g1(trav->lchild->data);
+	}
+	
+	
+}
+
+
